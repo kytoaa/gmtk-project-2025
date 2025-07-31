597df34a63256989969680228c41b4ece1acbfbf
amenthyst <pikadanielho@gmail.com>
2025-07-31 17:22:09 +0100

    fade transition

--- UI/transition_screen.gd
+++ UI/transition_screen.gd
@@ -1,1 +1,25 @@
 extends CanvasLayer
+
+@onready var color_rect = $ColorRect
+@onready var animation_player = $AnimationPlayer
+
+signal on_transition_finished
+# verbatim
+const FADE_TO_BLACK = "fade_to_black"
+const FADE_TO_NORMAL = "fade_to_normal"
+
+func _ready():
+	color_rect.visible = false
+	animation_player.animation_finished.connect(_on_animation_finished)
+
+func _on_animation_finished(anim_name):
+	if anim_name == FADE_TO_BLACK:
+		animation_player.play(FADE_TO_NORMAL)
+		on_transition_finished.emit()
+	elif anim_name == FADE_TO_NORMAL:
+		color_rect.visible = false
+	
+func play_transition():
+	color_rect.visible = true
+	animation_player.play(FADE_TO_BLACK)
+	
