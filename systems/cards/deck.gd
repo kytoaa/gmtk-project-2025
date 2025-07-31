83b0e7755fc51200e5368cd4f80b3d28c0704aac
kytoaa <58483576+kytoaa@users.noreply.github.com>
2025-07-31 14:09:58 +0200

    basic deck setup

---
+++ systems/cards/deck.gd
@@ -0,0 +1,49 @@
+class_name Deck
+extends Resource
+
+
+var cards: Array[Card]
+
+
+func _init() -> void:
+	var cards = []
+	for suit in CardSuit.values():
+		for card in CardType.values():
+			cards.append(Card.build(card, suit))
+	
+	self.cards = cards
+
+
+enum CardType {
+	NUMBER_1,
+	NUMBER_2,
+	NUMBER_3,
+	NUMBER_4,
+	NUMBER_5,
+	NUMBER_6,
+	NUMBER_7,
+	NUMBER_8,
+	NUMBER_9,
+	NUMBER_10,
+	NUMBER_11,
+	NUMBER_12,
+	NUMBER_13,
+}
+
+enum CardSuit {
+	A,
+	B,
+	C,
+	D,
+}
+
+
+class Card:
+	var type: CardType
+	var suit: CardSuit
+	
+	static func build(type: CardType, suit: CardSuit) -> Card:
+		var card = Card.new()
+		card.type = type
+		card.suit = suit
+		return card
