65195c93019c81b6ccbfe13cdc5fecceb1d3da11
kytoaa <58483576+kytoaa@users.noreply.github.com>
2025-07-31 16:45:46 +0200

    card changes

---
+++ entities/game_manager/game_manager.gd
@@ -0,0 +1,3 @@
+extends Node
+
+@onready var deck: Deck = Deck.new()
--- systems/cards/deck.gd
+++ systems/cards/deck.gd
@@ -1,6 +1,8 @@
 class_name Deck
 extends Resource
 
+const CardSuit = Card.CardSuit
+const CardType = Card.CardType
 
 var cards: Array[Card]
 
@@ -8,42 +10,13 @@ var cards: Array[Card]
 func _init() -> void:
 	var cards = []
 	for suit in CardSuit.values():
-		for card in CardType.values():
-			cards.append(Card.build(card, suit))
+		for type in CardType.values():
+			cards.append(Card.build(type, suit))
 	
 	self.cards = cards
 
+func count_suit(suit: CardSuit) -> int:
+	return len(self.cards.filter(func(card: Card): card.suit == suit))
 
-enum CardType {
-	NUMBER_1,
-	NUMBER_2,
-	NUMBER_3,
-	NUMBER_4,
-	NUMBER_5,
-	NUMBER_6,
-	NUMBER_7,
-	NUMBER_8,
-	NUMBER_9,
-	NUMBER_10,
-	NUMBER_11,
-	NUMBER_12,
-	NUMBER_13,
-}
-
-enum CardSuit {
-	A,
-	B,
-	C,
-	D,
-}
-
-
-class Card:
-	var type: CardType
-	var suit: CardSuit
-	
-	static func build(type: CardType, suit: CardSuit) -> Card:
-		var card = Card.new()
-		card.type = type
-		card.suit = suit
-		return card
+func count_type(type: CardType) -> int:
+	return len(self.cards.filter(func(card: Card): card.type == type))
